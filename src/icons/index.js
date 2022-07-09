9f4a46a66561372ba37de13b340d1a47d092b86d
catchychoruses <catchychoruses@gmail.com>
2022-07-09 05:20:01 +0200

    svg index

---
+++ src/icons/index.js
@@ -0,0 +1,19 @@
+import B_up from "./b_up.svg"
+import B_down from "./b_down.svg"
+import Play from "./play.svg"
+import Power from "./power.svg"
+import R from "./r.svg"
+import G from "./g.svg"
+import B from "./b.svg"
+import W from "./w.svg"
+import Blue_up from "./blue_up.svg"
+import Red_up from "./red_up.svg"
+import Green_up from "./green_up.svg"
+import Blue_down from "./blue_down.svg"
+import Red_down from "./red_down.svg"
+import Green_Down from "./green_down.svg"
+
+
+export const MySvg = {
+    B_up, B_down, Play, Power, R, G, B, W, Blue_down, Blue_up, Green_Down, Green_up, Red_up, Red_down
+}
